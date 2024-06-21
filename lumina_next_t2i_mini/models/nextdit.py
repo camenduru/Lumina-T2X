021e90e0250092a1c5f3856e7be445ececfe5cf4
camenduru <54370274+camenduru@users.noreply.github.com>
2024-06-21 06:28:28 +0300

    test

--- lumina_next_t2i_mini/models/nextdit.py
+++ lumina_next_t2i_mini/models/nextdit.py
@@ -13,10 +13,9 @@ import functools
 import math
 from typing import List, Optional, Tuple
 
-from flash_attn.bert_padding import index_first_axis, pad_input, unpad_input  # noqa
-
 # try:
 #     from flash_attn import flash_attn_varlen_func
+#     from flash_attn.bert_padding import index_first_axis, pad_input, unpad_input  # noqa
 
 #     is_flash_attn = True
 # except:
@@ -238,57 +237,57 @@ class Attention(nn.Module):
             return x_out.type_as(x_in)
 
     # copied from huggingface modeling_llama.py
-    def _upad_input(self, query_layer, key_layer, value_layer, attention_mask, query_length):
-        def _get_unpad_data(attention_mask):
-            seqlens_in_batch = attention_mask.sum(dim=-1, dtype=torch.int32)
-            indices = torch.nonzero(attention_mask.flatten(), as_tuple=False).flatten()
-            max_seqlen_in_batch = seqlens_in_batch.max().item()
-            cu_seqlens = F.pad(torch.cumsum(seqlens_in_batch, dim=0, dtype=torch.int32), (1, 0))
-            return (
-                indices,
-                cu_seqlens,
-                max_seqlen_in_batch,
-            )
+    # def _upad_input(self, query_layer, key_layer, value_layer, attention_mask, query_length):
+    #     def _get_unpad_data(attention_mask):
+    #         seqlens_in_batch = attention_mask.sum(dim=-1, dtype=torch.int32)
+    #         indices = torch.nonzero(attention_mask.flatten(), as_tuple=False).flatten()
+    #         max_seqlen_in_batch = seqlens_in_batch.max().item()
+    #         cu_seqlens = F.pad(torch.cumsum(seqlens_in_batch, dim=0, dtype=torch.int32), (1, 0))
+    #         return (
+    #             indices,
+    #             cu_seqlens,
+    #             max_seqlen_in_batch,
+    #         )
 
-        indices_k, cu_seqlens_k, max_seqlen_in_batch_k = _get_unpad_data(attention_mask)
-        batch_size, kv_seq_len, num_key_value_heads, head_dim = key_layer.shape
+    #     indices_k, cu_seqlens_k, max_seqlen_in_batch_k = _get_unpad_data(attention_mask)
+    #     batch_size, kv_seq_len, num_key_value_heads, head_dim = key_layer.shape
 
-        key_layer = index_first_axis(
-            key_layer.reshape(batch_size * kv_seq_len, num_key_value_heads, head_dim),
-            indices_k,
-        )
-        value_layer = index_first_axis(
-            value_layer.reshape(batch_size * kv_seq_len, num_key_value_heads, head_dim),
-            indices_k,
-        )
-        if query_length == kv_seq_len:
-            query_layer = index_first_axis(
-                query_layer.reshape(batch_size * kv_seq_len, self.n_local_heads, head_dim),
-                indices_k,
-            )
-            cu_seqlens_q = cu_seqlens_k
-            max_seqlen_in_batch_q = max_seqlen_in_batch_k
-            indices_q = indices_k
-        elif query_length == 1:
-            max_seqlen_in_batch_q = 1
-            cu_seqlens_q = torch.arange(
-                batch_size + 1, dtype=torch.int32, device=query_layer.device
-            )  # There is a memcpy here, that is very bad.
-            indices_q = cu_seqlens_q[:-1]
-            query_layer = query_layer.squeeze(1)
-        else:
-            # The -q_len: slice assumes left padding.
-            attention_mask = attention_mask[:, -query_length:]
-            query_layer, indices_q, cu_seqlens_q, max_seqlen_in_batch_q = unpad_input(query_layer, attention_mask)
+    #     key_layer = index_first_axis(
+    #         key_layer.reshape(batch_size * kv_seq_len, num_key_value_heads, head_dim),
+    #         indices_k,
+    #     )
+    #     value_layer = index_first_axis(
+    #         value_layer.reshape(batch_size * kv_seq_len, num_key_value_heads, head_dim),
+    #         indices_k,
+    #     )
+    #     if query_length == kv_seq_len:
+    #         query_layer = index_first_axis(
+    #             query_layer.reshape(batch_size * kv_seq_len, self.n_local_heads, head_dim),
+    #             indices_k,
+    #         )
+    #         cu_seqlens_q = cu_seqlens_k
+    #         max_seqlen_in_batch_q = max_seqlen_in_batch_k
+    #         indices_q = indices_k
+    #     elif query_length == 1:
+    #         max_seqlen_in_batch_q = 1
+    #         cu_seqlens_q = torch.arange(
+    #             batch_size + 1, dtype=torch.int32, device=query_layer.device
+    #         )  # There is a memcpy here, that is very bad.
+    #         indices_q = cu_seqlens_q[:-1]
+    #         query_layer = query_layer.squeeze(1)
+    #     else:
+    #         # The -q_len: slice assumes left padding.
+    #         attention_mask = attention_mask[:, -query_length:]
+    #         query_layer, indices_q, cu_seqlens_q, max_seqlen_in_batch_q = unpad_input(query_layer, attention_mask)
 
-        return (
-            query_layer,
-            key_layer,
-            value_layer,
-            indices_q,
-            (cu_seqlens_q, cu_seqlens_k),
-            (max_seqlen_in_batch_q, max_seqlen_in_batch_k),
-        )
+    #     return (
+    #         query_layer,
+    #         key_layer,
+    #         value_layer,
+    #         indices_q,
+    #         (cu_seqlens_q, cu_seqlens_k),
+    #         (max_seqlen_in_batch_q, max_seqlen_in_batch_k),
+    #     )
 
     def forward(
         self,
